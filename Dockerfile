6ea1437060121da6dd1267de461e84ca06e1ac84
deewai <deewai48@gmail.com>
2018-09-21 16:22:13 +0100

    updated install.sh

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,3 @@
 RUN docker-php-ext-install mysqli pdo pdo_mysql
+
+docker exec php /bin/sh -c '../order/api/vendor/bin/phpunit --bootstrap ../order/tests/bootstrap.php ../order/tests'
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod 777 -R ./
 >&2 echo "Building all containers...."
 docker-compose up -d;
 >&2 echo "Installing pdo_mysql...."
@@ -12,5 +13,5 @@ docker exec mysql /bin/sh -c 'mysql -P 3306 --protocol=tcp -u root -pmorerin </s
 >&2 echo "Running composer install..."
 docker exec php /bin/sh -c 'cd ../order/api;php composer.phar install;'
 >&2 echo "Running tests..."
-docker exec php /bin/sh -c '../order/api/vendor/bin/phpunit --bootstrap ../order/tests/bootstrap.php ../order/tests'
+
 >&2 echo "Application running at port 3128...."
