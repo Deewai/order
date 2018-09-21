3fff6076af80cb5930435f384640f51846297c28
deewai <deewai48@gmail.com>
2018-09-21 16:44:02 +0100

    removed google credentials

--- api/config/config.php
+++ api/config/config.php
@@ -1,11 +1,10 @@
 <?php
 CONST api_prefix = '';
 
-CONST api_key = 'AIzaSyD39RHtfY6sHMQBOkw1EUAmxH5VdEgaF7k';
+CONST api_key = '';
 define("__SERVER", $_ENV['MYSQL_PORT_3306_TCP_ADDR']);
 define("__USER", "root");
 define("__PASS", $_ENV['MYSQL_ENV_MYSQL_ROOT_PASSWORD']);
 define("__DBNAME", $_ENV['MYSQL_ENV_MYSQL_ROOT_DATABASE']);
-putenv('GOOGLE_APPLICATION_CREDENTIALS=./../ordertest_googleapi.json');
 
 ?>
